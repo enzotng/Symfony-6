3673e35047645cfa53c7e69f34f2bfbe2478ed20
smoukeuur <enzotang@live.fr>
2023-01-20 14:03:56 +0100

    Init Symfony

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body><h1>Lucky number</h1>: '.$number.'</body></html>'
+        );
+    }
+}
